583955e2d4e1bac352a9fb3a28b2462d1b03cd62
DianaOaxaca <hoaxacadiana@gmail.com>
2025-04-08 15:32:58 -0700

    add wf

--- install/workflow.R
+++ install/workflow.R
@@ -7,6 +7,7 @@ library(DiagrammeRsvg)
 #library(rsvg)
 
 # Create the diagram
+
 graph <- grViz("
 digraph workflow {
   
@@ -14,48 +15,52 @@ digraph workflow {
 
   node [shape = rectangle, style = filled, fontname = Helvetica, fontsize = 12, width = 3]
 
+  // Environment Setup
   subgraph cluster_0 {
     label = 'Environment Setup'
-    color = lightgray
-    style = filled;
-    node [fillcolor = lightblue]
+    color = white
+    style = invis;
+    node [fillcolor = lightblue, fontcolor = black]
     create_dirs [label = 'Create database directories\n(DBs/kegg, DBs/cazy, DBs/merops, DBs/iprscan)']
     conda_env [label = 'Create conda environment\n(rbimsenv)']
     install_tools [label = 'Install tools:\nKofamScan, dbCAN,\nInterProScan']
   }
 
+  // Database Preparation
   subgraph cluster_1 {
     label = 'Database Preparation'
-    color = lightgray
-    style = filled;
-    node [fillcolor = gold]
-    kofam_db [label = 'Download Kofam database']
-    dbcan_db [label = 'Download & build dbCAN database']
+    color = white
+    style = invis;
+    node [fillcolor = gold, fontcolor = black]
+    kofam_db [label = 'Download & config Kofam database']
+    dbcan_db [label = 'Download, build & config dbCAN database']
     merops_db [label = 'Download MEROPS protease.lib\n+ makeblastdb']
-    iprscan_db [label = 'Download InterProScan data\n(automatic with InterProScan install)']
+    iprscan_db [label = 'Download & config InterProScan data']
   }
 
+  // Annotation Workflows
   subgraph cluster_2 {
     label = 'Annotation Workflows'
-    color = lightgray
-    style = filled;
-    node [fillcolor = lightgreen]
+    color = white
+    style = invis;
+    node [fillcolor = lightgreen, fontcolor = black]
     interpro [label = 'Run InterProScan']
     kofam [label = 'Run KofamScan']
     dbcan [label = 'Run dbCAN']
     merops [label = 'Run MEROPS (BLASTp)']
   }
 
+  // Integration & Analysis
   subgraph cluster_3 {
     label = 'Integration & Analysis'
-    color = lightgray
-    style = filled;
-    node [fillcolor = lightpink]
+    color = white
+    style = invis;
+    node [fillcolor = lightpink, fontcolor = black]
     rbims [label = 'Integrate annotations in RbiMs']
     analysis [label = 'Downstream analysis\nand visualization']
   }
 
-  # Connections
+  // Connections
   create_dirs -> conda_env -> install_tools
   install_tools -> kofam_db
   install_tools -> dbcan_db
